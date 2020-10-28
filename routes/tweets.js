18d6dad977b0691eb14591afc9eaa6fa17fddcb1
Mathias <mathiasanderson88@gmail.com>
2020-10-28 13:15:47 -0700

    complete GET Tweets/:id

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db/models');
+const { Tweet } = db;
 
-router.get("/", (req, res) => {
-  res.json({message: 'test tweets index'})
-});
+const asyncHandler = (handler) => (req, res, next) =>
+    handler(req, res, next).catch(next);
+
+
+router.get(
+    "/",
+    asyncHandler(async (req, res) => {
+        const tweets = await Tweet.findAll()
+        res.json({ tweets })
+    })
+);
+
+
+const tweetNotFoundError = (id) => {
+    const err = new Error(`Tweet with the id of ${id} not found`);
+    err.title = "Tweet not found"
+    err.status = 404;
+    return (err);
+
+
+}
+router.get(
+    "/:id(\\d+)",
+    asyncHandler(async (req, res, next) => {
+        const tweetId = parseInt(req.params.id);
+        const tweet = await Tweet.findByPk(tweetId);
+        if(tweet){
+            res.json({ tweet })
+
+
+        } else {
+            next(taskNotFoundError(tweetId))
+        }
+
+    })
+)
+
+
 
 
 module.exports = router;
