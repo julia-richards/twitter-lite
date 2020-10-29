4d05ee0484c68c6ccc9db6d736eca5da8e9f4e10
Mathias <mathiasanderson88@gmail.com>
2020-10-29 12:32:25 -0700

    Setting up jwt

---
+++ express-sequelize-starter/auth.js
@@ -0,0 +1,6 @@
+const jwt = require('jsonwebtoken');
+const jwtConfig = require('./config');
+
+const { secret, expiresIn } = jwtConfig;
+
+
--- express-sequelize-starter/routes/users.js
+++ express-sequelize-starter/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
 const { asyncHandler, handleValidationErrors } = require('../utils');
+const { User } = require('../db/models')
 const check = require('express-validator');
 
 const validateUsername =
@@ -19,10 +21,19 @@ const validateEmailAndPassword = [
 ];
 
 
-router.post('/', validateUsername, validateEmailAndPassword, handleValidationErrors, asyncHandler(async((req, res) => {
+router.post('/',
+validateUsername,
+validateEmailAndPassword,
+handleValidationErrors,
+asyncHandler(async((req, res) => {
  const { username, email, password } = req.body;
- //TODO create user
+ const hashedPassword = await bcrypt.hash(password, 10);
+
+ const user = await User.create({ username, email, hashedPassword });
+
+
 }))
+)
 
 
 module.exports = router
